b3ee76214a6af5f4d29c6427e8d36d9e656eb2b0
max.fedrizzi <max.fedrizzi@buonarroti.tn.it>
2022-11-22 18:34:23 +0100

    tutto è cambiato, una nuova era è giunta

---
+++ src/Professore.java
@@ -0,0 +1,9 @@
+public class Professore {
+    private String name;
+    private int age;
+
+    public Professore(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
